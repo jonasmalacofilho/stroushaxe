ea4b40dbf14b21ed591dd9c7a047749a341bcf01
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-08-05 20:38:51 -0300

    Add: ExerciseOperators

---
+++ 03_04_exercise_operators/ExerciseOperators.hx
@@ -0,0 +1,15 @@
+class ExerciseOperators {
+    static function main()
+    {
+        Sys.println("Please enter a floating-point value:");  // `Sys.println` automatically adds the newline at the end
+        var n = Std.parseFloat(Sys.stdin().readLine());
+        Sys.println('n == $n');
+        Sys.println('n+1 == ${n+1}');
+        Sys.println('three times n == ${3*n}');
+        Sys.println('twice n == ${n+n}');
+        Sys.println('n squared == ${n*n}');
+        Sys.println('half of n == ${n/2}');
+        Sys.println('square root of n == ${Math.sqrt(n)}');
+    }
+}
+
