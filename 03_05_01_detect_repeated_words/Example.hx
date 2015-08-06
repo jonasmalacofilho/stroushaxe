39d6a940259f0d6cde1f358f2c569b264e3eedc0
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-08-05 21:47:51 -0300

    Add: DetectRepeatedWords

---
+++ 03_05_01_detect_repeated_words/Example.hx
@@ -0,0 +1,21 @@
+import Facilities.readWord;
+
+/**
+  Detect repeated words.
+**/
+class Example {
+    static function main()
+    {
+        var previous = " ";  // previous word, initialized not 'not a word'
+        var current;  // current word
+        try {  // keep reading a stream
+            while (true) {
+                current = readWord();
+                if (previous == current)  // check if the word is the same as last
+                    Sys.println('repeated word: $current');
+                previous = current;
+            }
+        } catch (e:haxe.io.Eof) {}  // until an exception of EOF (end of file) is raised
+    }
+}
+
